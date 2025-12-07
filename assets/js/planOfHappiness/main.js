72f1e6877bb23b216e5833c38704e63c1754617d
jm1021 <jmort1021@gmail.com>
2025-12-06 22:02:41 -0800

    little alignment change

--- assets/js/planOfHappiness/main.js
+++ assets/js/planOfHappiness/main.js
@@ -198,7 +198,7 @@ class GameLevelDesert {
     src: sprite_src_moon,
     SCALE_FACTOR: 4,
     ANIMATION_RATE: 100,
-    INIT_POSITION: { x: (width * 65 / 80), y: (height * 18 / 40)},
+    INIT_POSITION: { x: (width * 65 / 80), y: (height * 16 / 40)},
     orientation: {rows: 1, columns: 1 },
     hitbox: { widthPercentage: 0.1, heightPercentage: 0.1 },
     // Add dialogues array for random messages
@@ -233,7 +233,7 @@ class GameLevelDesert {
     src: sprite_src_stars,
     SCALE_FACTOR: 4,
     ANIMATION_RATE: 100,
-    INIT_POSITION: { x: (width * 65 / 80), y: (height * 30 / 40)},
+    INIT_POSITION: { x: (width * 65 / 80), y: (height * 28 / 40)},
     orientation: {rows: 1, columns: 1 },
     hitbox: { widthPercentage: 0.1, heightPercentage: 0.1 },
     // Add dialogues array for random messages
